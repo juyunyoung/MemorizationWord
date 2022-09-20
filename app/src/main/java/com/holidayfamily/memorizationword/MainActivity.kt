123ae7569aa6df2320357a54099bda366ec92129
juyunyoung <yunyoung.ju@gmail.com>
2022-09-20 12:46:15 +0900

    button event add

--- app/src/main/java/com/holidayfamily/memorizationword/MainActivity.kt
+++ app/src/main/java/com/holidayfamily/memorizationword/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val saveButton = findViewById<Button>(R.id.saveBtn)
+        val homeButton  = findViewById<button>(R.id.homeBtn)
+        val shuffleButton = findViewById<Button>(R.id.shuffleBtn)
+
+        saveButton.setOnClickListener{
+            //pageMove
+        }
+        homeButton.setOnClickListener{
+            //pageMove
+        }
+        shuffleButton.setOnClickListener{
+            //pageMove
+        }
     }
 }
