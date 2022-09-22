bf7f264fdd14524a00bbb500ca54503f0e5cae7a
juyunyoung <yunyoung.ju@gmail.com>
2022-09-22 12:45:33 +0900

    button layout add

--- app/src/main/java/com/holidayfamily/memorizationword/ResultActivity.kt
+++ app/src/main/java/com/holidayfamily/memorizationword/ResultActivity.kt
@@ -1,24 +1,24 @@
 package com.holidayfamily.memorizationword
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val saveButton = findViewById<Button>(R.id.saveBtn)
-        val homeButton = findViewById<button>(R.id.homeBtn)
+        val homeButton  = findViewById<button>(R.id.homeBtn)
         val shuffleButton = findViewById<Button>(R.id.shuffleBtn)
 
-        saveButton.setOnClickListener {
+        saveButton.setOnClickListener{
             val intent = Intent(this, ResultActivity::class.java)
         }
-        homeButton.setOnClickListener {
+        homeButton.setOnClickListener{
             //pageMove
         }
-        shuffleButton.setOnClickListener {
+        shuffleButton.setOnClickListener{
             //pageMove
         }
     }
