8922382f496cc0e1751047fe349236c7b5bcacfb
juyunyoung <yunyoung.ju@gmail.com>
2022-10-06 12:52:43 +0900

    button layout add

--- app/src/main/java/com/holidayfamily/memorizationword/MainActivity.kt
+++ app/src/main/java/com/holidayfamily/memorizationword/MainActivity.kt
@@ -4,11 +4,78 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private const val TAG_CALENDER = "homeFragment"
+private const val TAG_HOME = "home_fragment"
+private const val TAG_MY_PAGE = "my_page_fragment"
+private const val TAG_MY_PAGE = "my_page_fragment"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNaviBinding
+//https://mimisongsong.tistory.com/m/33
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setFragment(TAG_HOME, HomeFragment())
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.calenderFragment -> setFragment(TAG_CALENDER, CalenderFragment())
+                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
+                R.id.myPageFragment-> setFragment(TAG_MY_PAGE, MyPageFragment())
+            }
+            true
+        }
 
 
     }
+
+
+
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null){
+            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
+        }
+
+        val calender = manager.findFragmentByTag(TAG_CALENDER)
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
+
+        if (calender != null){
+            fragTransaction.hide(calender)
+        }
+
+        if (home != null){
+            fragTransaction.hide(home)
+        }
+
+        if (myPage != null) {
+            fragTransaction.hide(myPage)
+        }
+
+        if (tag == TAG_CALENDER) {
+            if (calender!=null){
+                fragTransaction.show(calender)
+            }
+        }
+        else if (tag == TAG_HOME) {
+            if (home != null) {
+                fragTransaction.show(home)
+            }
+        }
+
+        else if (tag == TAG_MY_PAGE){
+            if (myPage != null){
+                fragTransaction.show(myPage)
+            }
+        }
+
+        fragTransaction.commitAllowingStateLoss()
+    }
+
+
+
 }
